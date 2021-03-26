a13cc290818b3e241408b74570839636906d5c3e
André Felipe <andre.silva@compasso.com.br>
2021-03-26 00:08:17 -0300

    ADD: github api integration

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,32 +1,27 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem'
-
-const repositories = [
-  {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-  },
-  {
-    name: '2unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-  },
-  {
-    name: '3unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-  },
-]
-
+import '../styles/repositories.scss'
+import axios from 'axios'
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.github.com/orgs/rocketseat/repos')
+      .then(({ data }) => {
+        setRepositories(data)
+
+        console.log(repositories)
+    })
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
         {repositories.map(repository => (
-          <RepositoryItem repository={repository} />
+          <RepositoryItem key={repository.id} repository={repository} />
         ))}
       </ul>
     </section>
